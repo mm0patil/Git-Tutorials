d59706d46355d8e62cbf19332a09203021f6339f
Manoj Patil <mm0patil@gmail.com>
2022-01-17 17:17:28 +0530

    Add files via upload

---
+++ git repo/client/todo/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='card bg-info text-center text-light rounded-0'>
+            <h1 className="display-4">
+                <span className="text-dark mr-3">MERN</span>TO-DO List
+            </h1>
+        </div>
+    )
+}
